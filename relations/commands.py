9e08ea160158e63f189fd3867c18ff7670083978
CrispenGari <Crispengari@gmail.com>
2022-09-09 08:06:43 +0200

    mysql-relations

--- relations/commands.py
+++ relations/commands.py
@@ -13,9 +13,15 @@ GET_USER = "SELECT users.id, username, email, photoURL, phoneNumber FROM users L
 CREATE_POST = "INSERT INTO posts(title, url, userId) VALUES(%s, %s, %s);"
 CREATE_TAG = "INSERT INTO tags(tag) VALUES(%s);"
 GET_TAG_ID = "SELECT MAX(id) FROM tags;"
+GET_TAG_BY_NAME = "SELECT id FROM tags WHERE tag=%s;"
+
+POST_TAGS_CREATE = "INSERT INTO posts_tags(postId, tagId) VALUES(%s, %s);"
 
 GET_POST = """
 SELECT posts.id, username, email, profileId, title, url, userId, photoURL, phoneNumber FROM 
 posts LEFT JOIN users ON users.id = posts.userId LEFT JOIN profiles ON profiles.id = users.profileId
 WHERE users.id=%s;
 """
+GET_RECENT_POST = "SELECT MAX(id) FROM posts;"
+
+GET_POST_TAGS = """SELECT tagId, tag, id from posts_tags LEFT JOIN tags ON tags.id = posts_tags.tagId WHERE postId = %s"""
--- relations/main.py
+++ relations/main.py
@@ -46,24 +46,52 @@ def login():
 def create_post(userId:int):
     title = input("Enter Post Title: ").strip()
     url = input("Photo URL: ").strip()
-    tags = input("Enter post tags sep by space: ").strip().split(" ")
-    tags_ids = list()
-    for tag in tags:
-        pass
+    tags = input("Enter post tags sep by space: ").lower().strip().split(" ")
+    # ['#football', '#this']
+
     cursor.execute(CREATE_POST, (title, url, userId))
     conn.commit()
+    cursor.execute(GET_RECENT_POST)
+    post_id = cursor.fetchone()[0]
+    tags_ids = list()
+    for tag in tags:
+        cursor.execute(GET_TAG_BY_NAME, (tag, ))
+        _tag = cursor.fetchone()
+        if _tag is None:
+            # the tag is not in the database add it
+            cursor.execute(CREATE_TAG, (tag, ))
+            conn.commit()
+            cursor.execute(GET_TAG_BY_NAME, (tag,))
+            _tag = cursor.fetchone()
+            tags_ids.append(_tag[0])
+        else:
+            tags_ids.append(_tag[0])
+
+
+    for _tag_id in tags_ids:
+        cursor.execute(POST_TAGS_CREATE, (post_id, _tag_id))
+        conn.commit()
     print("Post Created!!")
 
 def get_post():
     id = int(input("User ID: ").strip())
     cursor.execute(GET_POST, (id, ))
-    data = [row for row in cursor]
-    tabulate_data(["postId"] + "username, email, profileId, title, url, userId, photoURL, phoneNumber".split(", "), data, "Post of User.")
+    ids = [d[0] for d in cursor]
+    cursor.execute(GET_POST, (id,))
+    data = [d for d in cursor]
+    tags = list()
+    for id in ids:
+        cursor.execute(GET_POST_TAGS, (id,))
+        tag = ", ".join([tag[1] for tag in cursor])
+        tags.append(tag)
+    data = [list(i) + [t] for i, t in zip(data, tags)]
+    tabulate_data(["postId"] + "username, email, profileId, title, url, userId, photoURL, phoneNumber, hashtags".split(", "), data, "Post of User.")
 
 if __name__ == "__main__":
-    # get_post()
+
     # register()
-    # create_post()
+    create_post(1)
     # login()
+    get_post()
     # close connection
     conn.close()
